939a9ed5434d40f7c24be2d85a3c2bba07629014
AneriMoradia0 <96448123+AneriMoradia0@users.noreply.github.com>
2024-03-11 23:48:49 -0500

    Unique Integer
    You are given an array of integers where each element appears twice except for one. Write a Java method to find and return the unique integer.

---
+++ ques5.java
@@ -0,0 +1,14 @@
+public class UniqueIntegerFinder {
+    public static int findUniqueInteger(int[] nums) {
+        int uniqueInteger = 0;
+        for (int num : nums) {
+            uniqueInteger ^= num;
+        }
+        return uniqueInteger;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2, 4, 6, 4, 2}; // Example array
+        int unique = findUniqueInteger(nums);
+        System.out.println("The unique integer is: " + unique);
+    }
+}
